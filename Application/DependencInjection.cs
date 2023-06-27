b368031f263ba9bb28d831476982934010f8a267
Chris Stone <chris.stone-ext@cygest.fr>
2023-06-27 21:02:59 +0400

    Add project files.

---
+++ Application/DependencInjection.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class DependencInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            var assembly = typeof(DependencInjection).Assembly;
+            services.AddMediatR(configuration => 
+            configuration.RegisterServicesFromAssembly(assembly));
+            services.AddValidatorsFromAssembly(assembly);
+            return services;
+
+        }
+    }
+}
