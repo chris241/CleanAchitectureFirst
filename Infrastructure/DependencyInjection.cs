eba493b65183a1f07457877bd69b0376b465da5d
Chris Stone <chris.stone-ext@cygest.fr>
2023-06-28 10:01:09 +0400

    finish

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -5,7 +5,7 @@ namespace Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApplication(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
 
             return services;
--- Presentation/DependencyInjection.cs
+++ Presentation/DependencyInjection.cs
@@ -4,7 +4,7 @@ namespace Presentation
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApplication(this IServiceCollection services)
+        public static IServiceCollection AddPresentation(this IServiceCollection services)
         {
 
             return services;
