dc4a20613a5238be239c4dfa15687c34f3195174
Chris Stone <chris.stone-ext@cygest.fr>
2023-06-28 10:01:29 +0400

    fix

--- WebUIX/Program.cs
+++ WebUIX/Program.cs
@@ -1,7 +1,15 @@
+using Application;
+using Infrastructure;
+using Presentation;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services
+    .AddApplication()
+    .AddInfrastructure()
+    .AddPresentation();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
